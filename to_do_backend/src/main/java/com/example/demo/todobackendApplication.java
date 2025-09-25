553668aa76f4c881c7d7961451a9fa90cd630a24
Kavia AI <production@kavia.ai>
2025-09-25 13:21:46 +0000

    CheckPoint - cge3a3dcfd

---
+++ to_do_backend/src/main/java/com/example/demo/todobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class todobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(todobackendApplication.class, args);
+	}
+
+}
